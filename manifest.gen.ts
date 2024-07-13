8991c48ffbc4e8d9081d5a40c2f8c6042bfac7ff
andriw aparecido <andriw1919@gmail.com>
2024-07-13 02:36:26 -0300

    framework flesh

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -4,21 +4,20 @@
 
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/Card.tsx";
 import * as $$$$$$1 from "./sections/Footer.tsx";
 import * as $$$$$$2 from "./sections/HelloSection.tsx";
-import * as $$$$$$3 from "./sections/ItWorks.tsx";
-import * as $$$$$$4 from "./sections/NewSection.tsx";
-import * as $$$$$$5 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$4 from "./sections/ItWorks.tsx";
+import * as $$$$$$5 from "./sections/NewSection.tsx";
+import * as $$$$$$6 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/Card.tsx": $$$$$$0,
+  
     "site/sections/Footer.tsx": $$$$$$1,
     "site/sections/HelloSection.tsx": $$$$$$2,
-    "site/sections/ItWorks.tsx": $$$$$$3,
-    "site/sections/NewSection.tsx": $$$$$$4,
-    "site/sections/Theme/Theme.tsx": $$$$$$5,
+    "site/sections/ItWorks.tsx": $$$$$$4,
+    "site/sections/NewSection.tsx": $$$$$$5,
+    "site/sections/Theme/Theme.tsx": $$$$$$6,
   },
   "apps": {
     "site/apps/deco/htmx.ts": $$$$$$$$$$$0,
--- sections/ItWorks.tsx
+++ sections/ItWorks.tsx
@@ -2,7 +2,7 @@ export default function Section() {
   return (
     <div class="p-10 grid place-items-center gap-2">
       <input id="toggle" type="checkbox" class="peer hidden" />
-      <label for="toggle" class="btn">Click Me</label>
+      <label for="toggle" class="btn">Click Me!</label>
       <div class="hidden peer-checked:inline">Hello!</div>
     </div>
   );
