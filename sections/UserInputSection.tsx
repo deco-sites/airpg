09e278c79e771277c1dc8b4b488776dc5eb6537d
andriw apareicdo <andriw1919@gmail.com>
2024-07-14 02:30:52 +0000

    Release web env environment with 1 changes made by andriw apareicdo

--- sections/UserInputSection.tsx
+++ sections/UserInputSection.tsx
@@ -1,14 +1,22 @@
 import { h } from "preact";
 import { useState } from "preact/hooks";
+import type { reqApi } from "../loaders/requestApiTest/requestApiTest.ts";
 
-interface Props {
-  /**
-   * @description The description of name.
-   */
-  name?: string;
+// Props type that will be configured in deco.cx's Admin
+export interface Props {
+  title: string;
+  req?: reqApi;
 }
-export default function Page (){
+
+export default function SectionUser(
+  { title, req }: Props,
+) {
   return (
-    <div>text</div>
+    <div class="p-4">
+      <h1 class="font-bold">{title}</h1>
+      <ul>
+        {req?.map((res) => <li>{res}</li>)}
+      </ul>
+    </div>
   );
-};
+}
