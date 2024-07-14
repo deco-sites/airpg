d9cf777466840d071f2d667953f9cda29e31cd90
andriw apareicdo <andriw1919@gmail.com>
2024-07-14 02:15:06 +0000

    Release web env environment with 0 changes made by

--- loaders/requestApiTest.tsx
+++ loaders/requestApiTest.tsx
@@ -1,17 +1,18 @@
-export reqApi = String[];
+export type reqApi = String[];
 
 interface Props {
   /**
   * @description The description of name.
   */
-  name?: string;
+  numberOfFacts?: BigInt;
 }
 
 export interface Returns {
   name: string
 }
 
-export async function loader({ name = "api" }: Props): Promise<reqApi> {
-  const { res } = awati();
+export async function loader({ numberOfFacts }: Props): Promise<reqApi> {
+  const { res } = (await fetch(`https://dogapi.dog/api/facts?number=${numberOfFacts ?? 1}`,
+  ).then((r) => r.json()));
   return res;
 }
