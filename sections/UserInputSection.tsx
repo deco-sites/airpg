b811f6510bf015b69fb45548afa40d163e715b38
andriw apareicdo <andriw1919@gmail.com>
2024-07-14 02:52:18 +0000

    Release web env environment with 0 changes made by

--- sections/UserInputSection.tsx
+++ sections/UserInputSection.tsx
@@ -1,5 +1,3 @@
-// import { h } from "preact";
-// import { useState } from "preact/hooks";
 import type { reqApi } from "../loaders/requestApiTest.ts";
 
 // Props type that will be configured in deco.cx's Admin
@@ -12,9 +10,6 @@ export default function SectionUser(
   { title, req }: Props,
 ) {
   return (
-    <div>
-      {req}
-    </div>
     <div class="p-4">
       <h1 class="font-bold">{title}</h1>
       <ul>
